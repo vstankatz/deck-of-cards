3ff3eef335fef58cfb80864e629ff58387f86c8b
Student <example@example.com>
2019-10-29 10:27:06 -0700

    adds for instead of forEach.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,27 @@
+
 $(document).ready(function(){
 
-var suits = ['clubs', 'hearts', 'diamonds', 'spades'];
-var numbers = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
-var deckCards = suits.forEach(function(suit){
-  var cards = numbers.forEach(function(number){
-    $(".results").append( number + " of " + suit + ", ");
 
-// var result = deckCards.toString();
-//   console.log(result);
+  // THIS IS THE FOREACH METHOD!!!///
+  var suits = ['clubs', 'hearts', 'diamonds', 'spades'];
+  var numbers = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+  var deckCards = suits.forEach(function(suit){
+    var cards = numbers.forEach(function(number){
+      $(".results").append( number + " of " + suit + ", ");
+
+      // var result = deckCards.toString();
+      //   console.log(result);
+    });
+    console.log(cards);
   });
-  console.log(cards);
-});
 
-
-
+  //THIS IS THE FOR METHOD!!!!///
+  // for (var i = 0; i < cards.length; i += 1) {
+  //   for (let j =0; j < suits.length; j +=1) {
+  //     $(".result").append((cards[i] + " of " + suits[j] + ", "));
+  //
+  //   }
+  // }
+  //^^^^^^^^^^^THIS IS THE FOR METHOD!!!!/// ^^^^^^^^
 
 });
